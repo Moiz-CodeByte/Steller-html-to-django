405f26617592ffc48d1d8ff99c8386df24f111a4
MoizCodeByte <hello@abdulmoiz.net>
2025-03-28 02:49:02 +0500

    setup urls and define views

--- StellerProject/stellerapp/urls.py
+++ StellerProject/stellerapp/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.steller, name='steller'),
+    path('home/', views.home, name='home')
 ]
--- StellerProject/stellerapp/views.py
+++ StellerProject/stellerapp/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 
 def steller(request):
     return HttpResponse("Hello world!")
+def home(request): 
+    return render(request, 'index.html')
