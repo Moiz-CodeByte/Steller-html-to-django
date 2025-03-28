beed53700dbb48da816aa8dd8720f0a07def64fb
MoizCodeByte <hello@abdulmoiz.net>
2025-03-29 03:36:15 +0500

    feat: Added contact, blog, portfolio, and testimonials pages

--- StellerProject/stellerapp/urls.py
+++ StellerProject/stellerapp/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     # path('', views.steller, name='steller'),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('blog/', views.blog, name='blog'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('testimonials/', views.testimonials, name='testimonials'),
 ]
--- StellerProject/stellerapp/views.py
+++ StellerProject/stellerapp/views.py
@@ -9,3 +9,13 @@ def home(request):
     return render(request, 'index.html')
 def about(request):
     return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def services(request):
+    return render(request, 'services.html')
+def blog(request):
+    return render(request, 'blog.html')
+def portfolio(request): 
+    return render(request, 'portfolio.html')
+def testimonials(request):
+    return render(request, 'testimonials.html')
