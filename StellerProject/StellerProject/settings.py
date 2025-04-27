eb19b3669ab92db5f4ddd8313429795fde3549a2
Abdul Moiz <hello@abdulmoiz.net>
2025-04-27 14:15:54 +0500

    Update settings.py

--- StellerProject/StellerProject/settings.py
+++ StellerProject/StellerProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4oj#w9td2=2)#m#cellpxr-bt41zd+jsj#lp90%i-vm9u(kzls
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['steller-django.vercel.app']
+ALLOWED_HOSTS = ['steller-django.vercel.app', 'steller-django-bqdxp3w25-abdul-moizs-projects-aaf06eae.vercel.app' , '.vercel.app' ]
 
 
 # Application definition
