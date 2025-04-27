a3d0b58a7a5c51e940ca905d1be92f9cffa2052a
Abdul Moiz <hello@abdulmoiz.net>
2025-04-27 15:00:59 +0500

    Update settings.py

--- StellerProject/StellerProject/settings.py
+++ StellerProject/StellerProject/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # folder where Django collects static
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'stellerapp/static'),  # where you keep your static before collecting
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
