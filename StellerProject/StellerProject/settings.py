69e3568bde52c8574ddeb85eb44e4f2ac03eb294
MoizCodeByte <hello@abdulmoiz.net>
2025-04-27 08:35:43 +0500

    chore: update settings.py for production deployment

--- StellerProject/StellerProject/settings.py
+++ StellerProject/StellerProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4oj#w9td2=2)#m#cellpxr-bt41zd+jsj#lp90%i-vm9u(kzls'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['steller-django.vercel.app']
 
 
 # Application definition
