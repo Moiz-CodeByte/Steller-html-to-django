d60f883a11bb1d3ce26cb3887c38ac1c0e8184f5
MoizCodeByte <hello@abdulmoiz.net>
2025-04-27 16:01:52 +0500

    Enhance static file handling: add debug info to build_staticfiles.sh, update vercel.json for cache control, and configure WhiteNoise in settings.py

--- StellerProject/StellerProject/settings.py
+++ StellerProject/StellerProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,12 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # folder where Django collects static
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Where Django looks for static files before collecting
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'stellerapp/static'),  # where you keep your static before collecting
+    os.path.join(BASE_DIR, 'stellerapp/static'),
 ]
+
+# Enable WhiteNoise's GZip
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- build_staticfiles.sh
+++ build_staticfiles.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# Debug information
+echo "Current directory: $(pwd)"
+ls -la
+
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --noinput 
+
+# Run collectstatic with noinput flag and verbosity
+python manage.py collectstatic --noinput --verbosity 2
+
+# Show contents of staticfiles directory
+echo "Contents of staticfiles directory:"
+ls -la staticfiles/
+ls -la staticfiles/css/ 
